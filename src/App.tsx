368697c06044760efd18ef8d41e2de79cd28893f
manikantag2k3 <manikantagandla2@gmail.com>
2024-11-06 21:32:37 +0530

    fixed link

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
         "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=2000",
       github: "https://github.com/manikantagandla2/hotel-booking",
       // Add your deployed link
-      deployedLink: "hotel-booking-frontend-x4z6.onrender.com/",
+      deployedLink: "https://hotel-booking-frontend-x4z6.onrender.com",
     },
   ];
 
