86bfab4ea54761488be448c1062d166c7486291b
manikantag2k3 <manikantagandla2@gmail.com>
2024-11-21 21:41:10 +0530

    link

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
       techStack: ["MongoDB", "Express", "React", "Node.js", "Stripe"],
       image:
         "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=2000",
-      github: "https://github.com/manikantagandla2/hotel-booking",
+      github: "https://github.com/manikantag2k3/hotel-booking",
       deployedLink: "https://hotel-booking-frontend-x4z6.onrender.com",
       // Add your deployed link
     },
